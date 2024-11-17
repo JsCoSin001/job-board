29b565d61f2edfaa742237c0194a3496d9e99c24
Linh <cosin.js@gmail.com>
2024-11-17 17:15:57 +0700

    123

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,3 +1,8 @@
 <x-layout>
-    <x-job-card :$job />
+    <x-breacrumbs class="mb-4"
+    :links="['Jobs' => route('jobs.index'), $job->title => '#']"
+    />
+    <x-job-card :$job> 
+        <p class="text-sm text-slate-500 mb-4" >{!! nl2br(e($job->description)) !!} </p>
+    </x-job-card>
 </x-layout>
